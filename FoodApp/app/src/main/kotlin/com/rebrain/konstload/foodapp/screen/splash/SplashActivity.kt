f30665e287fa0a5dfb9bd46d50cfbeba573f9b60
Константин <konstload@gmail.com>
2019-03-07 00:03:56 +0300

    add first logic

--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
@@ -1,13 +1,24 @@
 package com.rebrain.konstload.foodapp.screen.splash
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.rebrain.konstload.foodapp.MainActivity
 import com.rebrain.konstload.foodapp.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch {
+            delay(500)
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
     }
 }
