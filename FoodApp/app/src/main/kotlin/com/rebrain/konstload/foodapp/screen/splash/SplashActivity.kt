b96f4c0eedcbc210afe7b5e0c4820d85d6144c5b
Константин <konstload@gmail.com>
2019-03-06 23:59:41 +0300

    edit activity_splash layout

--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
@@ -1,7 +1,8 @@
-package com.rebrain.konstload.foodapp
+package com.rebrain.konstload.foodapp.screen.splash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.rebrain.konstload.foodapp.R
 
 class SplashActivity : AppCompatActivity() {
 
