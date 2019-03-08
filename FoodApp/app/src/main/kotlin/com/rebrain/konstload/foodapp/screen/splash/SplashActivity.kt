e48465a3f34ae43cd2251c78535debacd2b4f9b9
Константин <konstload@gmail.com>
2019-03-08 22:39:18 +0300

    fix

--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/splash/SplashActivity.kt
@@ -5,20 +5,25 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.rebrain.konstload.foodapp.MainActivity
 import com.rebrain.konstload.foodapp.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
+/**
+ * Экран загрузки во время запуска приложения, пока пиложение загружает некоторые базовые данные,
+ * что бы на первом экране был контент для просмотра пользователем.
+ **/
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        CoroutineScope(Dispatchers.Main).launch {
+            runNextActivity()
+        }
+    }
 
-        GlobalScope.launch {
+    private suspend fun runNextActivity(){
             delay(500)
             startActivity(Intent(applicationContext, MainActivity::class.java))
             finish()
-        }
     }
 }
