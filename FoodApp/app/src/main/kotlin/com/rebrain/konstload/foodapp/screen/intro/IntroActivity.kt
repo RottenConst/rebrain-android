913a5bb48e781979def930a5f695f2d081796bea
Константин <konstload@gmail.com>
2019-03-16 17:46:54 +0300

    update fix

--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/intro/IntroActivity.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/intro/IntroActivity.kt
@@ -3,7 +3,7 @@ package com.rebrain.konstload.foodapp.screen.intro
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.rebrain.konstload.foodapp.R
-import com.rebrain.konstload.foodapp.utils.notFirstRunApp
+import com.rebrain.konstload.foodapp.utils.runNotActivity
 
 /**
  * Интро-экран с кратким описанием возможностей приложения, запускается 1 раз, при первом запуске приложения.
@@ -13,6 +13,6 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-        notFirstRunApp(this)
+        runNotActivity(this)
     }
 }
--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/utils/PreferencesUtils.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/utils/PreferencesUtils.kt
@@ -4,19 +4,85 @@ import android.content.Context
 import com.rebrain.konstload.foodapp.R
 
 /**
-  * Утилитные методы для работы с SharedPreferences
-  **/
+ * Утилитный класс для работы с SharedPreferences
+ **/
+
+class PreferencesUtils(private val context: Context) {
+
+    fun getPref(res: Int): Any? {
+        return context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .all[res.toString()]
+    }
+
+    fun getPrefBool(res: Int, defValue: Boolean) = context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+        .getBoolean(res.toString(), defValue)
+
+
+    fun getPrefInt(res: Int, defValue: Int) = context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+        .getInt(res.toString(), defValue)
+
+
+    fun getPrefFloat(res: Int, defValue: Float) = context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+        .getFloat(res.toString(), defValue)
+
+
+    fun getPrefLong(res: Int, defValue: Long) = context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+        .getLong(res.toString(), defValue)
+
+    fun getPrefStringSet(res: Int, defValue: MutableSet<String>) =
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .getStringSet(res.toString(), defValue)
+
+
+    fun setPrefBoolean(res: Int, value: Boolean) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putBoolean(res.toString(), value)
+            .apply()
+    }
+
+    fun setPrefInt(res: Int, value: Int) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putInt(res.toString(), value)
+            .apply()
+    }
+
+    fun setPrefFloat(res: Int, value: Float) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putFloat(res.toString(), value)
+            .apply()
+    }
+
+    fun setPrefLong(res: Int, value: Long) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putLong(res.toString(), value)
+            .apply()
+    }
+
+    fun setPrefString(res: Int, value: String) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putString(res.toString(), value)
+            .apply()
+    }
+
+    fun setPrefStringSet(res: Int, value: MutableSet<String>) {
+        context.getSharedPreferences(res.toString(), Context.MODE_PRIVATE)
+            .edit()
+            .putStringSet(res.toString(), value)
+            .apply()
+    }
+}
 
 //возвращает true, если приложение запускается в первый раз(значение isFirst еще не задано)
 fun isFirstRunApp(context: Context): Boolean {
-    return context.getSharedPreferences(R.string.first_run.toString(), Context.MODE_PRIVATE)
-        .getBoolean(R.string.first_run.toString(), true)
+    return PreferencesUtils(context).getPrefBool(R.string.first_run, true)
 }
 
 //задает значению SharedPreferences isFirst - false
-fun notFirstRunApp(context: Context) {
-    context.getSharedPreferences(R.string.first_run.toString(), Context.MODE_PRIVATE)
-        .edit()
-        .putBoolean(R.string.first_run.toString(), false)
-        .apply()
+fun runNotActivity(context: Context) {
+    PreferencesUtils(context).setPrefBoolean(R.string.first_run, false)
 }
