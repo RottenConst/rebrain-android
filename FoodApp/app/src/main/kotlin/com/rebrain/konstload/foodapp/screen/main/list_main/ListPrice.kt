b376d30ab8dcd9f059134054987f45eae3760ad8
Konstantin <konstload@gmail.com>
2019-09-26 01:28:42 +0300

    add adapter

---
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/main/list_main/ListPrice.kt
@@ -0,0 +1,39 @@
+package com.rebrain.konstload.foodapp.screen.main.list_main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.rebrain.konstload.foodapp.R
+import com.rebrain.konstload.foodapp.domain.Product
+import kotlinx.android.synthetic.main.layout_list_item_product.view.*
+
+class ListPrice(
+    val products: MutableList<Product> = mutableListOf()
+) :
+    RecyclerView.Adapter<ListPrice.ListPriceHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListPriceHolder {
+        return ListPriceHolder(LayoutInflater.from(parent.context), parent)
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(holder: ListPriceHolder, position: Int) {
+        val product = products[position]
+        holder.bind(product)
+    }
+
+
+    class ListPriceHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(
+        inflater.inflate(R.layout.layout_list_item_product, parent, false)
+    ) {
+
+        fun bind(data: Product) {
+            itemView.text_for_product.text = data.name
+            itemView.text_for_price.text = data.id.toString()
+        }
+
+    }
+
+}
