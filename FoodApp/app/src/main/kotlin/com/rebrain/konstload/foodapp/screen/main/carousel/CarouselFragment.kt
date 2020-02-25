99f02eafca3367e084cc28377f58f1d93b299729
Konstantin <konstload@gmail.com>
2020-02-25 23:44:13 +0300

    fix carousel
    exeption при повороте

--- FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/main/carousel/CarouselFragment.kt
+++ FoodApp/app/src/main/kotlin/com/rebrain/konstload/foodapp/screen/main/carousel/CarouselFragment.kt
@@ -1,6 +1,5 @@
 package com.rebrain.konstload.foodapp.screen.main.carousel
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import androidx.core.os.bundleOf
 import com.bumptech.glide.Glide
 import com.rebrain.konstload.foodapp.R
 import com.rebrain.konstload.foodapp.base.BaseFragment
-import com.rebrain.konstload.foodapp.util.Generator
 import kotlinx.android.synthetic.main.fragment_carousel.*
 
 const val ARGUMENT_PAGE_NUMBER = "arg_page_number"
@@ -17,7 +15,7 @@ const val ARGUMENT_PAGE_NUMBER = "arg_page_number"
 /**
  * класс фрагмент для реализации карусели с картинками
  */
-class CarouselFragment(private val images: List<Int>) : BaseFragment() {
+class CarouselFragment : BaseFragment() {
 
     private var pageNumber = 0
 
@@ -30,7 +28,11 @@ class CarouselFragment(private val images: List<Int>) : BaseFragment() {
         pageNumber = arguments?.getInt(ARGUMENT_PAGE_NUMBER) ?: 0
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
         inflater.inflate(R.layout.fragment_carousel, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,8 +41,12 @@ class CarouselFragment(private val images: List<Int>) : BaseFragment() {
     }
 
     companion object {
+        private val images = mutableListOf<Int>()
+
         fun newInstance(page: Int, images: List<Int>): CarouselFragment {
-            val carouselFragment = CarouselFragment(images)
+            val carouselFragment = CarouselFragment()
+            this.images.clear()
+            this.images.addAll(images)
             val arg = bundleOf(ARGUMENT_PAGE_NUMBER to page)
             carouselFragment.arguments = arg
             return carouselFragment
